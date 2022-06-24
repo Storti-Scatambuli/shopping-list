3ee9d32f605faa06806a96079e8db0477fbf3713
João Guilherme Storti Scatambuli <jstorti87@gmail.com>
2022-06-24 20:05:16 -0300

    new class

--- assets/__init__.py
+++ assets/__init__.py
@@ -1,3 +1,39 @@
+class ListadeCompras():
+    lista = {}
+    arquivo = ''
+    
+    def __init__(self):
+        print('Qual arquivo de lista deseja utilizar?')
+        self.arquivo = input('Nome ou caminho do arquivo: ')
+        try:
+            with open(self.arquivo, 'r') as arq:
+                itens = arq.readlines()
+            for item in itens:
+                item = item.strip().split('.')
+                self.lista[item[0]] = item[-1]
+        except:
+            arq = open(self.arquivo, 'x')
+            arq.close()
+            
+    def atualizarLista(self, item=''):
+        item = item.strip().capitalize().split(':')
+        self.lista[item[0]] = item[1]
+        
+    def salvaLista(self):
+        try:
+            with open(self.arquivo, 'w') as arq:
+                for item in self.lista:
+                    arq.write(f'{item:.<50}{self.lista[item]}\n')
+        except:
+            print('Erro ao salvar a lista :(')
+        else:
+            print('Sua lista foi salva com sucesso!')
+                
+    def mostrarLista(self):
+        for item in self.lista:
+            print(f'{item:.<20}{self.lista[item]}')
+
+
 def titulo(texto, caracter='='):
     tamanho = len(texto) + 4
     print(caracter*tamanho)
@@ -20,38 +56,3 @@ def menu(titulo_texto, opcoes=[]):
             else:
                 print('Digite um valor dentro do intervalo das opções')
     return decisao
-
-def mostraItens(itens_dicionario):
-    for item in itens_dicionario:
-        print(f'{item:.<20}{itens_dicionario[item]}')
-        
-def atualizarItem(itens_dicionario):
-    print('Digite o nome do produto seguido por : e logo insira o valor que queira adicionar.\nNão se preocupe caso o item não esteja sendo exibido, digite o nome e ele será criado.\nDigite sair para voltar ao menu anterior e salvar as alterações.')
-    print()
-    itens_copia = itens_dicionario
-    while True:
-        mostraItens(itens_copia)
-        entrada = input('').strip().upper()
-        if entrada == 'SAIR':
-            break
-        elif ':' not in entrada:
-            print('Favor, digite um valor correto')
-        else:
-            entrada = entrada.split(':')
-            itens_copia[entrada[0]] = entrada[1]
-    
-def abreLista(dicionario, arquivo='lista.txt'):
-    try:
-        with open(arquivo, 'r') as arq:
-            itens = arq.readlines()
-        for item in itens:
-            item_lista = item.strip().split('.')
-            dicionario[item_lista[0]] = item_lista[-1]
-    except:
-        arq = open(arquivo, 'x')
-        arq.close()
-     
-def salvaLista(dicionario, arquivo='lista.txt'):
-    with open(arquivo, 'w') as arq:
-       for item in dicionario:
-           arq.write(f'{item:.<50}{dicionario[item]}\n') 
--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import assets
 
-lista = {}
-
-assets.abreLista(lista)
+lista = assets.ListadeCompras()
 
 while True:
     entrada = assets.menu('LISTA DE COMPRAS', ['Ver lista', 'Mudar lista', 'Salvar lista', 'Sair'])
     
     if entrada == 0:
-        assets.mostraItens(lista)
+        lista.mostrarLista()
+        
     if entrada == 1:
-        assets.atualizarItem(lista)
+        print('Digite o nome do item, seguido por : e logo sua quantidade com unidade, exemplo: Arroz: 20KG.\nDigite SAIR para sair.')
+        while True:
+            lista.mostrarLista()
+            entradaLista = input('>>> ').strip().upper()
+            if entradaLista == 'SAIR':
+                break
+            elif ':' not in entradaLista:
+                print('Modo incorreto de item')
+            else:
+                lista.atualizarLista(entradaLista)
+                
     if entrada == 2:
-        assets.salvaLista(lista)
+        lista.salvaLista()
+            
     if entrada == 3:
-        print('Obrigado, volte sempre.')
+        print('Você escolheu sair')
         break
-    
